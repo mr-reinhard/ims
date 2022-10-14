d059c03b47aea05cbab0e1727e0220eeeb0bed8e
mr-reinhard <sonycomputerentertaimentjapan@gmail.com>
2022-10-14 20:06:36 +0700

    inisial commit di branch fitur upload PP di master

--- modul/master/akun/informasi_pengguna.php
+++ modul/master/akun/informasi_pengguna.php
@@ -1,4 +1,14 @@
 <style>
+input[type="file"] {
+    display: none;
+}
+    .custom-file-upload {
+    display: inline-block;
+    padding: 6px 12px;
+    cursor: pointer;
+    border-radius: 100%;
+}
+
 .outer {
   width: 100% !important;
   height: 100% !important;
@@ -10,19 +20,14 @@
   }
   
 .inner {
-  width: 50px;
-  height: 50px;
+  width: 150px;
+  height: 150px;
   border-radius: 100%;
   position: absolute;
-  bottom: 10px;
-  right: 13px;
-}
-
-.btn{
-border-radius: 100%;
+  bottom: -100px;
+  right: -35px;
 }
 </style>
-
 <script>
     $(document).ready(function(){
             $(function () {
@@ -35,24 +40,29 @@ border-radius: 100%;
     <div class="container-fluid">
         <div class="row text-center">
             <div class="col-xl-10">
-                <h1 class="dash-title">
-                    <div class="outer">
-                        <img src="https://divedigital.id/wp-content/uploads/2022/07/10-Blank-Profile-Picture-with-Hat.jpg" class="rounded-circle img-thumbnail" alt="">
-                        <div class="inner">
-                            <button class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Rubah Foto">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                            </button>
-                        </div>
-                    </div>
-                    
-                </h1>
 
-                <h3 class="text-success">
-                    Docomo Z5
-                    <h1>
-                        MASTER USER
+                <form action="" method="post" enctype="multipart/form-data" >
+                    <h1 class="dash-title">
+                        <div class="outer">
+                            <img src="https://divedigital.id/wp-content/uploads/2022/07/10-Blank-Profile-Picture-with-Hat.jpg" class="rounded-circle img-thumbnail" alt="">
+                            <div class="inner">
+                                <label for="file_upload_foto_profile" class="custom-file-upload btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Ubah Foto">
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                </label>
+                            </div>
+                        </div>
                     </h1>
-                </h3>
+
+                    <input id="file_upload_foto_profile" type="file" name="name_foto_profile">
+
+                    <h3 class="text-success">
+                        Docomo Z5
+                        <h1>
+                            MASTER USER
+                        </h1>
+                    </h3>
+                </form>
+
             </div>
         </div>
     </div>
